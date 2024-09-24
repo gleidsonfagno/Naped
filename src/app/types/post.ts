8b73a526d5acd256a7de48ab65c602eb11f8bac7
gledsonfagno <gleidsonfagner838@gmail.com>
2024-09-23 21:28:22 -0300

    Adiciona tipagem de dados para Post na pasta types

---
+++ src/app/types/post.ts
@@ -0,0 +1,17 @@
+export interface Post {
+    id: number;
+    imageUrl: string;
+    category: string;
+    title: string;
+    resume: string;
+    date: string;
+    content: string;
+    star: string;
+    new: string;
+  }
+
+  export interface ISearch {
+    title: string;
+    imageUrl: string;
+    category: string;
+  }
