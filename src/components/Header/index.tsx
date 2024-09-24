9ebc7d5b7ece93f445937377301b9954a877946d
gledsonfagno <gleidsonfagner838@gmail.com>
2024-09-24 20:43:31 -0300

    adicionnado componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,66 @@
+'use client';
+// import { ButtonText } from "../ButtonText";
+
+import { Cabecalho, Container, Nav, Span, MenuBar } from "./styles";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+import { RiMenuLine } from "react-icons/ri";
+import { RiCloseLine } from "react-icons/ri";
+
+export default function Header() {
+  // controlar a abertura do menu
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // funçao para alterar o  menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <Container>
+      <Cabecalho>
+        <Span>
+          <Image
+            src={"/Logo_naped.png"}
+            width={40}
+            height={40}
+            alt="logo naped"
+          />
+          <strong>Naped</strong>
+        </Span>
+
+        <Nav menuOpen={menuOpen}>
+          <ul className={menuOpen ? "menu-open" : ""}>
+            <li>
+              <Link href="/">Inicio</Link>
+            </li>
+            <li>
+              <Link href="/categories/Series">Séries</Link>
+            </li>
+            <li>
+              <Link href="/categories/Filmes">Filmes</Link>
+            </li>
+            <li>
+              <Link href="/categories/Animes">Animes</Link>
+            </li>
+            <li>
+              <Link href="/categories/Jogos">Jogos</Link>
+            </li>
+            <li>
+              <MenuBar className="open_menu" onClick={toggleMenu}>
+                {menuOpen ? <RiCloseLine /> : <RiMenuLine />}
+              </MenuBar>
+            </li>
+            {/* <ButtonText title="Minha conta" to="/Signin" /> */}
+          </ul>
+        </Nav>
+
+        <MenuBar className="open_menu" onClick={toggleMenu}>
+          {menuOpen ? <RiCloseLine size={25} /> : <RiMenuLine size={25} />}
+        </MenuBar>
+      </Cabecalho>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+
+interface menuProps {
+    menuOpen: boolean
+}
+
+export const Container = styled.div`
+  border-bottom: 1px solid ${(props) => props.theme.BORDA};
+  /* border-bottom-style: solid; */
+`;
+export const Cabecalho = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+  max-width: 1168px;
+  margin: 0 auto;
+`;
+
+export const Span = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+
+  > strong {
+    color: ${(props) => props.theme.BUTTOM};
+  }
+  > img {
+    display: block;
+    width: 40px;
+    height: 45px;
+  }
+`;
+
+export const Nav = styled.nav<menuProps>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 79px;
+  > ul {
+    display: flex;
+    flex-direction: row;
+    gap: 57px;
+    align-items: center;
+
+    li {
+      list-style-type: none;
+
+      a {
+        color: ${(props) => props.theme.WHITE};
+        &:hover {
+          color: ${(props) => props.theme.BUTTOM};
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 935px) {
+    align-items: left;
+    justify-content: left;
+    flex-direction: column;
+    overflow: hidden;
+    ul {
+        position: fixed;
+    
+      display: flex;
+      flex-direction: column;
+      top: 0;
+      z-index: 3;
+      left: ${(props) =>
+        props.menuOpen ? "0" : "-100%"}; /* Deslizar da esquerda */
+      /* right: 0; */
+      bottom: 0;
+      align-items: center;
+      text-align: center;
+      padding-top: 40px;
+      width: 50%;
+      height: 100%;
+
+      background-color: ${(props) => props.theme.DARCK_80};
+    }
+  }
+`;
+
+export const MenuBar = styled.button`
+  display: none;
+  background: transparent;
+  border: none;
+  > svg {
+    /* margin-left: 16px; */
+    color: ${(props) => props.theme.WHITE};
+  }
+  @media screen and (max-width: 935px) {
+    display: block;
+  }
+`;
